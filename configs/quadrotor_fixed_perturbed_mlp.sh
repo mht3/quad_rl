cbf72b70231b15a2ce9813811307029e506c5fbb
mht3 <mhtaylor31@gmail.com>
2025-12-03 16:19:28 -0800

    updated configs

--- configs/quadrotor_fixed_perturbed_mlp.sh
+++ configs/quadrotor_fixed_perturbed_mlp.sh
@@ -1,3 +1,1 @@
-python main.py --env_id Quadrotor-Fixed-Perturbed-v0 --algorithm PPO --seed 42 -t 10000000 --n_steps 3072 --batch_size 256 --lr 0.0001 --policy_net 512 256 128 --value_net 512 256 128 --perturbation_std 0.05
-python main.py --env_id Quadrotor-Fixed-Perturbed-v0 --algorithm PPO --seed 42 -t 10000000 --n_steps 3072 --batch_size 256 --lr 0.00005 --policy_net 512 256 128 --value_net 512 256 128 --perturbation_std 0.05
-
+python main.py --env_id Quadrotor-Fixed-Perturbed-v0 --algorithm PPO --seed 42 -t 30000000 --n_steps 6144 --batch_size 512 --lr 0.00002 --policy_net 512 256 128 --value_net 512 256 128 --perturbation_std 0.05
